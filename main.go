46386ed7feb1eb4a90168fc41f58691f874e7a29
SaYMooN <shokoladnysaymoon@mail.ru>
2023-12-19 19:52:42 +0300

    continued to set the basics of the project

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 
 var tasks = make([]src.Task, 0)
 var mutex = &sync.Mutex{}
-var tasksTemplate = template.Must(template.ParseFiles("templates/tasks.tmpl"))
+var tasksTemplate = template.Must(template.ParseFiles("templates/tasks.go.tmpl"))
 
 func main() {
+	http.Handle("/src_front/", http.StripPrefix("/src_front/", http.FileServer(http.Dir("src_front"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "src_front/index.html")
 	})
@@ -22,7 +23,7 @@ func main() {
 		}
 		newTask := src.Task{
 			Name:     r.FormValue("name"),
-			Info:     r.FormValue("info"),
+			Info:     r.FormValue("dfds"),
 			Complete: false,
 		}
 		mutex.Lock()
--- src_back/task.go
+++ src_back/task.go
@@ -1,7 +1,44 @@
 package src
 
+import (
+	"time"
+)
+
 type Task struct {
-	Name     string
-	Info     string
-	Complete bool
+	Id          uint
+	Name        string
+	Info        string
+	Complete    bool
+	HasDeadline bool
+	Deadline    time.Time
+	Importance  uint
+}
+
+func NewTask(id uint, name string, info string) Task {
+	return Task{
+		Id:          id,
+		Name:        name,
+		Info:        info,
+		HasDeadline: false,
+		Deadline:    time.Now(),
+	}
+}
+
+func NewTaskWithDeadline(id uint, name string, info string, deadline time.Time) Task {
+	return Task{
+		Id:          id,
+		Name:        name,
+		Info:        info,
+		HasDeadline: true,
+		Deadline:    deadline,
+	}
+}
+func (t *Task) SetImportance(importance uint) {
+	if importance < 1 {
+		t.Importance = 1
+	} else if importance > 10 {
+		t.Importance = 10
+	} else {
+		t.Importance = importance
+	}
 }
